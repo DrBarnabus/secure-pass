3996a8acc49b19f997cab7da98431f2c89e45ae4
DrBarnabus <16451892+DrBarnabus@users.noreply.github.com>
2018-09-10 18:36:19 +0100

    Fix test definition in Base64.

--- src/__tests__/base64.ts
+++ src/__tests__/base64.ts
@@ -2,7 +2,7 @@ import { bufferFromSafeBase64, bufferToSafeBase64 } from '..';
 
 describe('base64', () => {
   describe('bufferFromSafeBase64', () => {
-    test('Should convert a valid base64 string into a buffer.', () => {
+    test('Should convert a valid url-safe base64 string into a buffer.', () => {
       const expected = Buffer.from('TestString');
       const buffer = bufferFromSafeBase64('VGVzdFN0cmluZw');
       expect(buffer.compare(expected)).toEqual(0);
@@ -10,8 +10,10 @@ describe('base64', () => {
   });
 
   describe('bufferToSafeBase64', () => {
-    const expected = 'VGVzdFN0cmluZw';
-    const base64 = bufferToSafeBase64(Buffer.from('TestString'));
-    expect(base64).toEqual(expected);
+    test('Should convert a buffer into a valid url-safe base64 string.', () => {
+      const expected = 'VGVzdFN0cmluZw';
+      const base64 = bufferToSafeBase64(Buffer.from('TestString'));
+      expect(base64).toEqual(expected);
+    });
   });
 });
